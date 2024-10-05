5e829feeac9f3323acefd9480607d6b8baaa0f91
Team Defenestrate <teamdefenestrate@pissmail.com>
2024-10-05 23:53:30 +0200

    web: improve code comments

--- web/build.py
+++ web/build.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from os import path
+# parent is the directory containing the file
 webdir = Path(__file__).parent
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -20,17 +21,20 @@ try:
 except ImportError:
     from yaml import Loader, Dumper
 
-# load all group data into groups, with filename w/o extension as key
+# load all group data into groups, with stem (filename w/o extension) as key
 groups = {p.stem: load(open(p.name), Loader) for p in grouppaths}
 
+# write the list of groups (left iframe in index.html)
 listtpl = env.get_template("list.html")
 htmlfile = open(webdir.joinpath("list.html"), "w")
 htmlfile.write(listtpl.render(groups=groups))
 
+# json schema is used to validate the yaml files
 from jsonschema import Draft202012Validator
 from jsonschema.exceptions import ValidationError, SchemaError
 validator = Draft202012Validator(schema)
 grouptpl = env.get_template("group.html")
+# render each group's page
 for gid, group in groups.items():
     # https://python-jsonschema.readthedocs.io/en/stable/validate/
     messages = [e.message for e in validator.iter_errors(group)]
